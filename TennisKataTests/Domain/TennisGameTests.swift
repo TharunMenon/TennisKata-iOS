540bb0fae41f9e4bc7973d0ffede24c2a1b427b4
Tharun Menon <Tharunmenon@192.168.1.2>
2026-02-10 21:03:32 +0530

    RED - Add failing test for player1 scoring first point

--- TennisKataTests/Domain/TennisGameTests.swift
+++ TennisKataTests/Domain/TennisGameTests.swift
@@ -30,10 +30,21 @@ final class TennisGameTests: XCTestCase {
         let score = game.getScore()
         
         //Then: Score should be "Love All"
-        
         XCTAssertEqual(score, "Love All")
     }
 
-    
+    //MARK:-Test3:Player1 scores first point
+    func testPlayer1ScoresFirstPoint() throws{
+        //Given: A new tennis game
+        let game = TennisGame(player1:"Arjun",player2:"Anil")
+        
+        //When: Player1 scores a point
+        game.scorePlayer1()
+        
+        
+        //Then: Score should be "Fifteen Love"
+        XCTAssertEqual(game.getScore(), "Love All")
+        
+    }
 
 }
